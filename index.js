4dcad7d7be68ddebbc71ac24725a4556a62f7b70
abel-gg <abelchurriana@gmail.com>
2021-10-14 23:43:57 +0200

    version 1

---
+++ index.js
@@ -0,0 +1,42 @@
+const API_KEY    = "711f5cb83b018c032b337b4f98134c43";
+const fetchData = position => {
+    const { latitude, longitude } = position.coords;
+    fetch(`http://api.openweathermap.org/data/2.5/weather?units=metric&lat=${latitude}&lon=${longitude}&appid=${API_KEY}`)
+        .then(response => response.json())
+        .then(data => setWeatherData(data))
+    
+}
+
+const setWeatherData = data => {
+    const weatherData = {
+        location: data.name,
+        description: data.weather[0].main,
+        humidity: data.main.humidity,
+        pressure: data.main.pressure,
+        temperature: data.main.temp,
+        date: getDate()
+    }
+
+    Object.keys(weatherData).forEach(key => {
+        document.getElementById(key).textContent =  weatherData[key];
+    });
+
+    cleanUp();
+}
+
+const cleanUp = () => {
+    let container = document.getElementById('container');
+    let loader = document.getElementById('loader');
+
+    loader.style.display = 'none';
+    container.style.display = 'flex';
+}
+
+const getDate = () => {
+    let date = new Date();
+    return `${date.getDate()}-${('0' + (date.getMonth() + 1)).slice(-2)}-${date.getFullYear()}`;
+}
+
+const onLoad = () => {
+    navigator.geolocation.getCurrentPosition(fetchData);
+}
